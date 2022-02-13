720db61b701cc85dc4b10de1569ac227f4ba6777
WmongeF <waltermongee81@gmail.com>
2022-02-13 12:45:55 -0600

    v0.01

---
+++ src/vista/main.java
@@ -0,0 +1,11 @@
+package vista;
+
+public class main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        num_aleatorios prin=new num_aleatorios();
+         prin.show();
+    }   
+}
